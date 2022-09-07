4fe9592717696467dbcf385cee5f47917fbce22e
mostafizur610 <mostafizurrahman273@gmail.com>
2022-09-08 04:28:14 +0600

    get weather update

--- js/app.js
+++ js/app.js
@@ -9,11 +9,24 @@ const loadTemperature = city => {
 }
 
 const displayTemperature = data => {
-    const temperature = document.getElementById('temperature');
+    // const temperature = document.getElementById('temperature');
     // console.log(data.main.temp);
-    temperature.innerText = data.main.temp;
+    // temperature.innerText = data.main.temp;
+    setInnerTextById('temperature', data.main.temp);
+    setInnerTextById('condition', data.weather[0].main);
+    // console.log(data.weather[0].main);
 
 }
+const setInnerTextById = (id, text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+}
 
+document.getElementById('btn-search').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    document.getElementById('city').innerText = city;
+    loadTemperature(city);
+})
 
 loadTemperature('Dhaka')
